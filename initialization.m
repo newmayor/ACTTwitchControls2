66adef0373751ffdb2d6ffd1685c587a21070f9f
sonia-g <sgonzalez@ucdavis.edu>
2014-10-08 20:33:04 -0700

    initialization-Description, Results, Parameters

--- initialization.m
+++ initialization.m
@@ -1,3 +1,16 @@
+% Parameters: No parameters entered. 
+% Results: Intilization of the pin outs to the arduino and the arm is sent to 
+% its home position. The user sees a display from the script that tells them
+% that the arm is being sent to its home position.
+% Descritption:Script declares global variables for the length of the
+% upper arm and forearm, the angle of the horizontal imaginary arm, the angle 
+% of the vertical arm, the boundaries of the arms, and the maximum speed for the
+% pots found in the inner and outer arms and sets their values. The connection with 
+% the arduno is established and intializes the pin outs by calling the 
+% initTwitch() function. Then the arm is set to its default or home position
+% by calling the CenterArm() function and the script diplays to that the arm is
+% being sent to its home position.
+
 global L2 L3 theta1 theta4
 global x_f y_f
 global theta1_min theta2_min theta1_max theta2_max s1_max s2_max
