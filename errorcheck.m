29699eb394c352495819fef9d34cfd37f6096b10
sonia-g <sgonzalez@ucdavis.edu>
2014-10-09 00:15:23 -0700

    More Descriptions

--- errorcheck.m
+++ errorcheck.m
@@ -1,3 +1,14 @@
+% Parameters:None
+% Results: Error Message displayed to screen.
+% Descritpition: Sets the er value to false.Function checks to see 
+% if the point can be reached by the arm by calling the function withinreach().
+% Checks to see if the individual angles, the angle created by the upper arm 
+% and forearm or the angle created by the x- axis and the upper arm are within
+% bounds by calling the function anglebounds().Checks to see if the final 
+% destination is within the boundaries of the arm by calling the function 
+% simulation(). If any function called reports a true value then the appropriate
+% error message is displayed to the user.
+
 function er = errorcheck()
 
 er = 0;
--- finalpos.m
+++ finalpos.m
@@ -1,3 +1,9 @@
+% Parameters:User is prompted to input desired ending x and y coordinates
+% of arm.
+% Results: Saves the final x and y coordinates of the arm as global variables.
+% Description: Globalizes variables. Prompts the user for the final x-coordinate
+% and final y-coordinate of the arm.
+
 global L2 L3 theta1 theta4
 global x_f y_f
 global theta1_min theta2_min theta1_max theta2_max s1_max s2_max
