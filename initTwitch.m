0c1fbe943acaf317e3a356bc270592ca132922cc
sonia-g <sgonzalez@ucdavis.edu>
2014-10-08 20:06:01 -0700

    initTwitch- Description, Results, Parameters

--- initTwitch.m
+++ initTwitch.m
@@ -1,3 +1,13 @@
+% Parameters: no inputs needed 
+% Results: motor is prepped to be driven 
+% Descritption: Function begins by globalizing variables for turning the motors
+% on and off (motor1 and motor2) and for the dedicated pins into the arduino 
+% for sending commands and receiving information from the pot and motor. Pin locations 
+% are set for the motors. Motor 1:PIN_motor1_en=4;PIN_motor1_pwm1=5;
+% PIN_motor1_pwm2=6. Motor 2:PIN_motor2_en=8;PIN_motor2_pwm1=9;
+% PIN_motor2_pwm2=10. The potentiometer pins are set. Pot 1: PIN_pot1=0;
+% PIN_pot2=1. The function connects to the arduino and sets the pins as either
+% inputs or outputs.The motors are set to have a zero duty and are disabled
 function initTwitch()
 
 global a MOTOR1 MOTOR2 MOTORMAX PIN_motor1_en PIN_motor1_pwm1 PIN_motor1_pwm2 PIN_motor2_en PIN_motor2_pwm1 PIN_motor2_pwm2 PIN_pot1 PIN_pot2
