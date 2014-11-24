79a36abfe112f923614ff23c3cd9425698d10ad3
Numair Ahmed <numairahmed1@gmail.com>
2014-11-23 23:47:55 -0800

    Finalized velocity test function
    Replaced pseudo code with actual arduino commands to communicate with
    motors and logic board.

--- velocityTestFunc.m
+++ velocityTestFunc.m
@@ -1,51 +1,107 @@
-% 
-% 
-% 
-%% Set Experiment Loop
 
-%%
-% Set arm to default position of known value -- possibly horizontal to
-% positive x-axis.
+global a MOTOR1 MOTOR2 PIN_pot1 PIN_pot2
 
-%% Joint 1 - Forward
-% Isolate Joint 1 and sweep through entire range of motion while timing the
-% motion
+MinVoltage = 20;
+MaxVoltage = 0;
 
-tic
+InitialDuty = 0;
+MaxDuty = 1;
+DutyStep = 0.1;  % half volt increments
 
-duty = 0.5; %initial duty cycle value -- arbitrary [-1:1]
-DriveMotor(Motor1, duty);
+% leave these angle boundary conditions in units of degrees since
+% conversion from pot resistance values to degrees is done when storing
+% pot1 value in code below
+pot1Min = 0;
+pot1Max = 180;
+pot2Min = 0;
+pot2Max = 180;
 
-% Print all initial and final variable values (ie. change of angle,
-% voltage, end time)
-%% Joint 2 - Forward
-% Isolate Joint 2 and sweep through entire range of motion while timing the
-% motion
-
-
-% Print all initial and final variable values (ie. change of angle,
-% voltage, end time)
-
-%% Joint 1 - Backward
-% Isolate Joint 1 and sweep through entire range of motion backwards while timing the
-% motion
+t1min = 260;
+t1max = 990;
+t2min = 180;
+t2max = 890;
 
 
-% Print all initial and final variable values (ie. change of angle,
-% voltage, end time)
+duty = InitialDuty;
 
-%% Joint 2 - Backward
-% Isolate Joint 2 and sweep through entire range of motion backwards while timing the
-% motion
+% Initialization of the arm positions
+pot1 = (a.analogRead(PIN_pot1)-t1min)/(t1max-t1min)*180-90;
+pot1_buffer = 0.05*pot1; % let buffer resistance value be 5% of pot read value
+
+% while pot1 read value is greater than -95 percent of minimum pot value, proceed with
+% following logic statement
+% Motor movement direction may not be correct
+while pot1 >= pot1Min-pot1_buffer
+    DriveMotor(MOTOR1, -0.5);
+    pot1 = (a.analogRead(PIN_pot1)-t1min)/(t1max-t1min)*180-90;
+end
+DriveMotor(MOTOR1, 0);
+
+pot2 = (a.analogRead(PIN_pot2)-t2min)/(t2max-t2min)*180-90;
+pot2_buffer = 0.05*pot2; % let buffer resistance value be 5% of pot read value
+while pot2 >= pot2Min-pot2_buffer
+    DriveMotor(MOTOR2, -0.5);
+    pot1 = (a.analogRead(PIN_pot2)-t2min)/(t2max-t2min)*180-90;
+end
+DriveMotor(MOTOR2, 0);
 
 
-% Print all initial and final variable values (ie. change of angle,
-% voltage, end time)
-
-
-%% Excel Export
-% Save all relevant variables
-% Plot duty vs average angular velocities for each motor -- forward and
-% backward
-% Export to excel -- excelwrite()
-
+timeValues = zeros(size(11,4));
+% Beginning of the test loop
+for duty = 0:0.1:1
+    i = 0;
+    
+    
+    % Joint 1: Forward Direction
+    tic
+    while pot1 < pot1Max-pot1_buffer
+        DriveMotor(MOTOR1, duty);
+        pot1 = (a.analogRead(PIN_pot1)-t1min)/(t1max-t1min)*180-90;
+    end
+    DriveMotor(MOTOR1, 0);
+    time = toc;
+    fprintf('Joint 1 (forward motion) time elapsed: %d seconds using a %d duty cycle\n\n', time, duty)
+    timeValues(i,1) = time; % store time value
+    
+    
+    % Joint 2: Forward Direction
+    tic
+    pot2_buffer = 0.05*pot2; % let buffer resistance value be 5% of pot read value
+    while pot2 < pot2Max-pot2_buffer
+        DriveMotor(MOTOR2, duty);
+        pot1 = (a.analogRead(PIN_pot2)-t2min)/(t2max-t2min)*180-90;
+    end
+    DriveMotor(MOTOR2, 0);
+    time = toc;
+    fprintf('Joint 2 (forward motion) time elapsed: %d seconds using a %d duty cycle\n\n', time, duty)
+    timeValues(i,2) = time; % store time value
+    
+    
+    % Joint 1: Reverse Direction
+    tic
+    while pot1 > pot1Min+pot1_buffer
+        DriveMotor(MOTOR1, duty);
+        pot1 = (a.analogRead(PIN_pot1)-t1min)/(t1max-t1min)*180-90;
+    end
+    DriveMotor(MOTOR1, 0);
+    time = toc;
+    fprintf('Joint 1 (backward motion) time elapsed: %d seconds using a %d duty cycle\n\n', time, duty)
+    timeValues(i,3) = time; % store time value
+    
+    
+    % Joint 2: Reverse Direction
+    tic
+    pot2_buffer = 0.05*pot2; % let buffer resistance value be 5% of pot read value
+    while pot2 > pot2Min+pot2_buffer
+        DriveMotor(MOTOR2, -duty);
+        pot1 = (a.analogRead(PIN_pot2)-t2min)/(t2max-t2min)*180-90;
+    end
+    DriveMotor(MOTOR2, 0);
+    time = toc;
+    fprintf('Joint 2 (backward motion) time elapsed: %d seconds using a %d duty cycle\n\n', time, duty)
+    timeValues(i,4) = time; % store time value
+    
+    
+    
+    i = i+1;
+end
