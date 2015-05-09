0dce28b2bf81949c111893b6801c6baddde04067
Michelle <iammichelleau@hotmail.com>
2015-05-08 18:41:21 -0700

    Clean up center_arm, init_twitch.

---
+++ Four Bar Links/center_arm.m
@@ -0,0 +1,67 @@
+function center_arm()
+
+global a
+global L2 L3 theta1 theta4 x_i y_i x_f y_f 
+global er_ori curr_ori target_ori
+global theta2_min theta3_min theta2_max theta3_max s2_max s3_max
+global motor1 motor2 motor_max
+global pin_motor1_en pin_motor2_en pin_motor1_pwm1 pin_motor2_pwm1 pin_motor1_pwm2 pin_motor2_pwm2
+global pin_pot1 pin_pot2
+
+DriveMotor(motor1,0.5)
+DriveMotor(motor2,0.5)
+
+% Initializes the arm position at the start of the simulation
+% Sweep the range of motion
+pot1_stat = 0;
+pot2_stat = 0;
+
+while(~pot1_stat || ~pot2_stat)
+    fprintf('PIN0: %d, PIN1: %d\n',a.analogRead(PIN_pot0),a.analogRead(PIN_pot1)); 
+    
+    if(a.analogRead(pin_pot1) < 400)
+        disp('moving motor 1');
+        %disp('PIN1: '); disp(a.analogRead(PIN_pot0));
+        DriveMotor(motor1,0);
+        pot1_stat = 1;
+    end
+    if(a.analogRead(pin_pot2) > 800)
+        disp('moving motor 2');
+        %disp('PIN2: '); disp(a.analogRead(PIN_pot1));
+        DriveMotor(motor2,0);
+        pot2_stat = 1;
+    end
+end
+
+pot1_stat = 0;
+pot2_stat = 0;
+DriveMotor(motor1,-0.5);
+DriveMotor(motor2,-0.5);
+
+while(~pot1_stat || ~pot2_stat)
+    if(a.analogRead(pin_pot1) > 300)
+        DriveMotor(motor1,0);
+        pot1_stat = 1;
+    end
+    if(a.analogRead(pin_pot2) < 500)
+        DriveMotor(motor2,0);
+        pot2_stat = 1;
+    end
+end
+    
+% Go to 90 degrees
+pot1_stat = 0;
+pot2_stat = 0;
+DriveMotor(motor1,0.5);
+DriveMotor(motor2,0.5);
+
+while(~pot1_stat || ~pot2_stat)
+    if(a.analogRead(pin_pot1) < 400)
+        DriveMotor(motor2,0);
+        pot1_stat=1;
+    end
+    if(a.analogRead(pin_pot2) > 1000)
+        DriveMotor(motor2,0);
+        pot2_stat=1;
+    end
+end
--- Four Bar Links/init_twitch.m
+++ Four Bar Links/init_twitch.m
@@ -1,4 +1,4 @@
-function initTwitch()
+function init_twitch()
 
 global a
 global L2 L3 theta1 theta4 x_i y_i x_f y_f 
@@ -11,43 +11,42 @@ global pin_pot1 pin_pot2
 delete(instrfind({'Port'},{'/dev/tty.usbmodem1411'}));
 port = '/dev/tty.usbmodem1411';
 
-
-MOTORMAX = 157;
-MOTOR0 = 0;
-MOTOR1 = 1;
+motor_max = 157;
+motor1 = 0;
+motor2 = 1;
 
 % Motor 0
-PIN_motor0_en = 4;
-PIN_motor0_pwm1=5;
-PIN_motor0_pwm2=6;
+pin_motor1_en = 4;
+pin_motor1_pwm1 = 5;
+pin_motor1_pwm2 = 6;
 
 % Motor 1
-PIN_motor1_en=8;
-PIN_motor1_pwm1=9;
-PIN_motor1_pwm2=10;
+pin_motor2_en = 8;
+pin_motor2_pwm1 = 9;
+pin_motor2_pwm2 = 10;
 
 % Potentiometer 1
-PIN_pot0=0;
-PIN_pot1=1;
+pin_pot1 = 0;
+pin_pot2 = 1;
 
 % Connect to the board 
 a = arduino(port);
 
 % Set pin direction
-a.pinMode(PIN_motor0_en,'output');
-a.pinMode(PIN_motor0_pwm1,'output');
-a.pinMode(PIN_motor0_pwm2,'output');
+a.pinMode(pin_motor1_en,'output');
+a.pinMode(pin_motor1_pwm1,'output');
+a.pinMode(pin_motor1_pwm2,'output');
 
-a.pinMode(PIN_motor1_en,'output');
-a.pinMode(PIN_motor1_pwm1,'output');
-a.pinMode(PIN_motor1_pwm2,'output');
+a.pinMode(pin_motor2_en,'output');
+a.pinMode(pin_motor2_pwm1,'output');
+a.pinMode(pin_motor2_pwm2,'output');
 
-% init motors disabled
-a.digitalWrite(PIN_motor0_en, 1);
-a.digitalWrite(PIN_motor1_en, 1);
+% Init motors disabled
+a.digitalWrite(pin_motor1_en, 1);
+a.digitalWrite(pin_motor2_en, 1);
 
-% init motors at zero duty
-a.analogWrite(PIN_motor0_pwm1, 0);
-a.analogWrite(PIN_motor0_pwm2, 0);
-a.analogWrite(PIN_motor1_pwm1, 0);
-a.analogWrite(PIN_motor1_pwm2, 0);
+% Init motors at zero duty
+a.analogWrite(pin_motor1_pwm1, 0);
+a.analogWrite(pin_motor1_pwm2, 0);
+a.analogWrite(pin_motor2_pwm1, 0);
+a.analogWrite(pin_motor2_pwm2, 0);
